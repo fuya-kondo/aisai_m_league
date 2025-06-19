9067088592dfca77fce6f4082dcdb3b00f0ae249
f-kondo <f-kondo@voltage.co.jp>
2025-06-19 14:44:21 +0900

    サーバー切り替え

--- config/google_gemini.php
+++ config/google_gemini.php
@@ -2,8 +2,6 @@
 use GeminiAPI\Client;
 use GeminiAPI\Resources\Parts\TextPart;
 
-require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
-
 $api_key = 'AIzaSyBPPLGL-RgI732EuVylSSFNaIGr2NRQYJ4';
 $model = 'gemini-1.5-flash';
 $api_version = 'v1';
